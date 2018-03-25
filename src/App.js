bf69a76d65627bee51b1e3d1ca451a26572a5fb6
Souvik Banerjee <banerjeesouvik18@hotmail.co.uk>
2018-03-26 00:16:30 +0530

    Add forecast section

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ class App extends Component {
       lat : 12.970000,
       lon : 77.589996,
     }
-    this.update = this.update.bind(this);
+    this.update = this.update.bind(this)
   }
   update(cityInfo){
     //console.log('triggered with', cityName)
-    this.setState({city:cityInfo.name, lat : cityInfo.lat, lon : cityInfo.lon});
+    this.setState({city:cityInfo.name, lat : cityInfo.lat, lon : cityInfo.lon, day : 'today'});
   }
 
   render() {
@@ -28,26 +28,18 @@ class App extends Component {
   }
 }
 
-class Header extends Component {
-  constructor(){
-    super();
-    this.state = {
-      heading : 'Weather'
-    }
-  }
-  render(){
-    return (
-      <div className='heading'>{this.state.heading}</div>
-    )
-  }
-}
+const Header = () =>
+      <div className='heading'>
+        <div className='heading-logo'></div>
+        Weather<span className='heading-last'>.app</span>
+      </div>;
 
 class Search extends Component {
   constructor(){
     super();
     this.state = {
       city : '',
-      isResult: false,
+      isResult: 'default',
       result:''
     }
     this.update = this.update.bind(this);
@@ -57,13 +49,14 @@ class Search extends Component {
     var cityName = event.target.value;
     //console.log(event.keyCode)
     if(cityName.length > 0){
+      this.setState({isResult : 'loading'});
       fetch(`http://localhost:8080/weather?loc=${cityName}`)
         .then(response => response.json())
         .then(response => {
           //console.log(response);
           scope.setState({
             city : cityName,
-            isResult: true,
+            isResult: 'done',
             result: response.RESULTS.slice(0,5)
           })
           //console.log(this.state.isResult)
@@ -71,6 +64,7 @@ class Search extends Component {
     }
   }
   sendCity = () => {
+    console.log(this.state.result)
     let cityInfo = this.state.result.map(function(city){
       return {
         name : city.name,
@@ -78,13 +72,18 @@ class Search extends Component {
         lon : city.lon
       }
     });
+    console.log(cityInfo)
     cityInfo = cityInfo.find(city => {
       return city.name === this.state.city;
     });
-    this.props.updateParent(cityInfo)
+    console.log(cityInfo);
+    if(cityInfo !== undefined){
+      this.props.updateParent(cityInfo);
+    }
   }
   render(){
-    var res = this.state.isResult ? <SearchResult res={this.state.result}/> : undefined;
+    var res = this.state.isResult === 'done' ? <SearchResult res={this.state.result}/> : (this.state.isResult === 'loading') ?
+                <SuggestionLoad /> : undefined ;
     //console.log(this.state.isResult)
     return (
       <div className='search'>
@@ -95,6 +94,7 @@ class Search extends Component {
     )
   }
 }
+const SuggestionLoad = () => <div className = 'suggestion-load'></div>
 
 class SearchResult extends Component {
   render() {
@@ -114,11 +114,21 @@ class SearchResult extends Component {
 }
 
 class Navbar extends Component {
+  updateDay(day){
+    this.props.updateDay(day);
+  }
   render(){
+    let todaySelected = 'nav-bar-elm';
+    let forecastSelected = 'nav-bar-elm';
+    if(this.props.day === 'today'){
+      todaySelected += ' selected-tab';
+    }else{
+      forecastSelected += ' selected-tab';
+    }
     return (
       <div className='nav-bar'>
-        <div className='nav-bar-elm' onClick={() => this.props.day('today')}>Today</div>
-        <div className='nav-bar-elm' onClick={() => this.props.day('tomorrow')}>Tomorrow</div>
+        <div className={todaySelected} onClick={() => this.updateDay('today')}>Current Weather</div>
+        <div className={forecastSelected} onClick={() => this.updateDay('forecast')}>Forecast</div>
       </div>
     )
   }
@@ -129,95 +139,112 @@ class Weather extends Component {
     super();
     this.state = {
       weatherInfo : [],
+      forecast : [],
       isDataFetched : false,
-      day : ''
+      day : 'today'
     }
     this.updateDay = this.updateDay.bind(this);
   }
-  updateDay(clickedDay){
-    this.setState({day : clickedDay});
+  updateDay(val){
+    this.setState({day : val});
   }
   componentWillReceiveProps(nextProps){
+    this.setState({isDataFetched : false});
     if (nextProps.city.city !== this.props.city.city){
       //console.log(nextProps.city)
-      fetch(`http://api.wunderground.com/api/3b051654317f7634/forecast/q/${nextProps.city.lat},${nextProps.city.lon}.json`)
+      fetch(`http://api.wunderground.com/api/3b051654317f7634/conditions/q/${nextProps.city.lat},${nextProps.city.lon}.json`)
         .then(response => response.json())
-        .then(response => {
+        .then(weatherInfo => {
           //console.log(response);
-          this.setState({weatherInfo : response, isDataFetched : true})
+          fetch(`http://api.wunderground.com/api/3b051654317f7634/forecast/q/${nextProps.city.lat},${nextProps.city.lon}.json`)
+            .then(response => response.json())
+            .then(forecast => {
+              this.setState({weatherInfo : weatherInfo, forecast : forecast, isDataFetched : true, day: 'today'})
+            })
         })
     }
-  }
-  shouldComponentUpdate(nextProps, nextState){
-    return this.state.day !== nextState.day || this.props.city.city !== nextProps.city.city;
   }
   componentDidMount(){
-      fetch(`http://api.wunderground.com/api/3b051654317f7634/conditions/q/${this.props.city.lat},${this.props.city.lon}.json`)
-        .then(response => response.json())
-        .then(response => {
-          this.setState({weatherInfo : response, isDataFetched : true, day : 'today'})
-        })
+    fetch(`http://api.wunderground.com/api/3b051654317f7634/conditions/q/${this.props.city.lat},${this.props.city.lon}.json`)
+      .then(response => response.json())
+      .then(weatherInfo => {
+        console.log(weatherInfo);
+        fetch(`http://api.wunderground.com/api/3b051654317f7634/forecast/q/${this.props.city.lat},${this.props.city.lon}.json`)
+          .then(response => response.json())
+          .then(forecast => {
+            this.setState({weatherInfo : weatherInfo, forecast : forecast, isDataFetched : true})
+          })
+      })
   }
-  componentWillUpdate(){
-    console.log(this.state.day)
-    if(this.state.day === 'today'){
-      fetch(`http://api.wunderground.com/api/3b051654317f7634/conditions/q/${this.props.city.lat},${this.props.city.lon}.json`)
-        .then(response => response.json())
-        .then(response => {
-          this.setState({weatherInfo : response})
-        })
-    }else if(this.state.day === 'tomorrow'){
-      fetch(`http://api.wunderground.com/api/3b051654317f7634/forecast/q/${this.props.city.lat},${this.props.city.lon}.json`)
-        .then(response => response.json())
-        .then(response => {
-          this.setState({weatherInfo : response})
-        })
-    }
-  }
+
   render(){
-    let data = this.state.weatherInfo;
-    console.log(data)
+    let weather = this.state.weatherInfo;
+    let forecast = this.state.forecast;
+    //console.log(forecast)
+    let display;
+    if(this.state.day === 'today'){
+      display = <Today weather = {weather} />;
+    }else if(this.state.day === 'forecast'){
+      display = forecast.forecast.simpleforecast.forecastday.slice(1,4).map((forcst) => {
+        return <Forecast key = {forcst.date.weekday} forecast = {forcst} />;
+      });
+    }
     if(this.state.isDataFetched) {
-      if(this.state.day === 'today'){
         return(
           <div>
             <div className='city-info'>
-              <div className='city'>City : {data.current_observation.display_location.city}</div>
-              <div className='state'>State : {data.current_observation.display_location.state}</div>
-              <div className='country'>Country : {data.current_observation.display_location.state_name}</div>
+              <div>City : <span className='city'>{weather.current_observation.display_location.city}</span></div>
+              <div>State : <span className='state'>{weather.current_observation.display_location.state}</span></div>
+              <div>Country : <span className='country'>{weather.current_observation.display_location.state_name}</span></div>
             </div>
-            <Navbar day={this.updateDay}/>
-            <div className='weather-info'>
-              <div className='weather-type'>
-                <div className='weather'>Weather : {data.current_observation.weather}</div>
-                <img className='weather-img' src={data.current_observation.icon_url} alt={data.current_observation.icon}></img>
+            <Navbar updateDay={this.updateDay} day={this.state.day}/>
+              <div className='weather-info'>
+                {display}
               </div>
-              <div className='temp'>Temperature : {data.current_observation.temp_c}&deg; C</div>
-            </div>
           </div>
         )
-      }else{
-        return(
-          <div>
-            <Navbar day={this.updateDay}/>
-            <div className='weather-info'>
-              <div className='weather-type'>
-                <div className='weather'>Weather : {data.simpleforecast.forecastday[2].conditions}</div>
-                <img className='weather-img' src={data.forecast.forecastday[2].icon_url} alt={data.forecast.forecastday[2].icon}></img>
-              </div>
-              <div className='temp'>High Temperature : {data.simpleforecast.forecastday[2].high.celsius}&deg; C</div>
-              <div className='temp'>Low Temperature : {data.simpleforecast.forecastday[2].low.celsius}&deg; C</div>
-            </div>
+      }else {
+        return (
+          <div className='weather-info'>
+            <div className='loading'></div>
           </div>
         )
       }
-    }else {
-      return (
-        <div className='weather-info'>
-          <div className='loading'></div>
-        </div>
-      )
-    }
   }
+}
+
+const Today = (props) => {
+  return (
+    <div className='current-weather-wrapper'>
+      <div className='wrapper-small'>
+        <img className='weather-img' src={props.weather.current_observation.icon_url} alt={props.weather.current_observation.icon}></img>
+        <div className='weather'><span className='val'>{props.weather.current_observation.weather}</span></div>
+      </div>
+      <div className='temp'><span className='val'>{props.weather.current_observation.temp_c}&deg;C</span>Temperature</div>
+      <div className='humidity'><span className='val'>{props.weather.current_observation.relative_humidity}</span>Humidity</div>
+    </div>
+  )
+}
+
+const Forecast = (props) => {
+  return (
+    <div className='forecast-wrapper'>
+        <div className='forecast-day'>
+          <span className='week-day'>{props.forecast.date.weekday_short},</span>
+          <span className='month'>{props.forecast.date.monthname}</span>
+          <span className='date'>{props.forecast.date.day},</span>
+          <span className='year'>{props.forecast.date.year}</span>
+        </div>
+        <div className='forecast-type'>
+           <div className='forecast-weather'>Weather : {props.forecast.conditions}</div>
+           <img className='forecast-weather-img' src={props.forecast.icon_url} alt={props.forecast.icon}></img>
+        </div>
+        <div className='forecast-temp'>
+            Temperature : <b>(High) </b>{props.forecast.high.celsius} &deg;C ,
+                          <b> (Low) </b>{props.forecast.low.celsius} &deg;C
+        </div>
+        <div className='forecast-humidity'>Humidity : {props.forecast.avehumidity}%</div>
+    </div>
+  )
 }
 export default App;
